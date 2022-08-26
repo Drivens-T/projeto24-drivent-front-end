09946aded67f370c65632b01805535ef89d356c3
alyson <consulialyson@gmail.com>
2022-08-26 19:00:10 -0300

    feat: component cardPayment

---
+++ src/pages/Dashboard/Payment/CardPayment.js
@@ -0,0 +1,21 @@
+import PaymentForm from '../../../components/PaymentForm';
+import { OptionCard, PageInformation } from '.';
+
+export default function CardPayment() {
+  const card = {
+    title: 'Presencial + Com Hotel',
+    price: 'R$ 600',
+  };
+
+  return (
+    <>
+      <PageInformation>Ingresso escolhido</PageInformation>
+      <OptionCard large>
+        <h6>{card.title}</h6>
+        <p>{card.price}</p>
+      </OptionCard>
+      <PageInformation>Pagamento</PageInformation>
+      <PaymentForm></PaymentForm>
+    </>
+  );
+}
--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,38 +1,129 @@
-import { Typography } from '@material-ui/core';
 import styled from 'styled-components';
-import PaymentForm from '../../../components/PaymentForm';
+import { useState } from 'react';
+import { StyledTypography } from './../../../components/PersonalInformationForm';
+import CardPayment from './CardPayment';
 
 export default function Payment() {
+  const cardsExample = [
+    {
+      title: 'Presencial',
+      price: 150,
+    },
+    {
+      title: 'Online',
+      price: 300,
+    },
+  ];
+
   return (
-    <PaymentContainer>
+    <>
       <StyledTypography variant="h4">Ingresso e pagamento</StyledTypography>
-      <StyledTypography variant="h6">Ingresso escolhido</StyledTypography>
-      <SelectedBox></SelectedBox>
-      <StyledTypography variant="h6">Pagamento</StyledTypography>
-      <PaymentForm></PaymentForm>
-    </PaymentContainer>
+      <StepChoiceContainer>
+        {true ? (
+          <>
+            <PageInformation>Primeiro, escolha sua modalidade de ingresso</PageInformation>
+            <CardsRow cards={cardsExample}></CardsRow>
+          </>
+        ) : (
+          <CardPayment />
+        )}
+      </StepChoiceContainer>
+    </>
   );
 }
 
-const PaymentContainer = styled.div`
+export function PageInformation({ children }) {
+  return (
+    <>
+      <StyledPageInformation>{children}</StyledPageInformation>
+    </>
+  );
+}
+
+function CardsRow({ cards }) {
+  const [chosenCard, setChosenCard] = useState('');
+
+  function handleCardClick(cardTitle) {
+    setChosenCard({ ...chosenCard, title: cardTitle });
+  }
+
+  return (
+    <>
+      <StyledCardsRow>
+        {cards.map((card) => {
+          return (
+            <OptionCard
+              selected={card.title === chosenCard}
+              key={card.title}
+              onClick={() => handleCardClick(card.title)}
+            >
+              <h6>{card.title}</h6>
+              <p>{card.price}</p>
+            </OptionCard>
+          );
+        })}
+      </StyledCardsRow>
+    </>
+  );
+}
+
+export function OptionCard({ children, large, ...otherProps }) {
+  return (
+    <>
+      {large ? (
+        <LargeOptionContainer {...otherProps}>{children}</LargeOptionContainer>
+      ) : (
+        <SquareOptionContainer {...otherProps}>{children}</SquareOptionContainer>
+      )}
+    </>
+  );
+}
+
+const StepChoiceContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 17px;
+`;
+
+const StyledPageInformation = styled.h5`
+  font-size: 20px;
+  font-family: 'Roboto', sans-serif;
+  color: #8e8e8e;
+`;
+
+const StyledCardsRow = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 24px;
+`;
+
+const SquareOptionContainer = styled.div`
+  width: 145px;
+  height: 145px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 3px;
+  border: 1px solid #cecece;
+  border-radius: 20px;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 400;
+  cursor: pointer;
   h6 {
-    color: #8e8e8e;
+    font-size: 16px;
+    color: #454545;
+  }
+  p {
+    font-size: 14px;
+    color: #898989;
   }
 `;
 
-const StyledTypography = styled(Typography)`
-  margin-bottom: 20px !important;
-`;
-
-const SelectedBox = styled.div`
-  background-color: #ffeed2;
+const LargeOptionContainer = styled(SquareOptionContainer)`
   width: 290px;
   height: 108px;
-  border-radius: 20px;
-  margin-bottom: 20px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  gap: 8px;
+  background-color: #ffeed2;
+  border: none;
+  cursor: default;
 `;
