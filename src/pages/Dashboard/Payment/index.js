9697220efcd4fc3091e3bd89c0ac8cf86e6f6d3f
Mariana Ito <marianaitopinto@gmail.com>
2022-08-26 21:23:57 -0300

    feat: check if enrollment informations are complete

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,23 @@
+import useEnrollment from '../../../hooks/api/useEnrollment';
+import styled from 'styled-components';
+
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+  const { enrollment } = useEnrollment();
+
+  return enrollment ? (
+    <div>'Pagamento: Em breve!'</div>
+  ) : (
+    <Message>
+      <p>Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso</p>
+    </Message>
+  );
 }
+
+const Message = styled.div`
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 23px;
+  align-items: center;
+  margin: auto;
+  color: #8e8e8e;
+`;
