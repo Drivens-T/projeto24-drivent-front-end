4625c1f82d8dcdaff07a208ef98d9145a6671d23
Leonardo Lucas <leolucasguedes@yahoo.com.br>
2022-08-31 14:36:07 -0300

    feat: online ticket activities

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,42 @@
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import styled from 'styled-components';
+
+import useBookTicket from '../../../hooks/api/useBookTicket';
+
+import { StyledTypography } from './../../../components/PersonalInformationForm';
+import Message from '../../../components/Message';
+
 export default function Activities() {
-  return 'Atividades: Em breve!';
+  const { bookTicket } = useBookTicket();
+
+  function renderActivitiesStep() {
+    <h1>Tudo Ok</h1>;
+  }
+
+  function verifyModality() {
+    return (
+      <>
+        <StyledTypography variant="h4">Escolha de atividades</StyledTypography>
+        {bookTicket.modalityId === 1 ? (
+          <Message>
+            Sua modalidade de ingresso não necessita escolher atividade. Você terá acesso a todas as atividades.
+          </Message>
+        ) : (
+          renderActivitiesStep()
+        )}
+      </>
+    );
+  }
+
+  return (
+    <>
+      <StyledTypography variant="h4">Escolha de atividades</StyledTypography>
+      {!bookTicket.paid ? (
+        <Message>Você precisa ter confirmado pagamento antes de fazer a escolha de atividades</Message>
+      ) : (
+        verifyModality()
+      )}
+    </>
+  );
 }
